f6da53a986d96667361bd85aedfeefd55107b1b7
shimanski <shimanski.alex@gmail.com>
2022-01-28 21:47:18 +0200

    rename

--- src/main/java/com/rsinnotech/keycloakdemo/web/ApiController.java
+++ src/main/java/com/rsinnotech/keycloakdemo/web/ApiController.java
@@ -14,7 +14,7 @@ import java.util.Collections;
 import java.util.Map;
 
 @RestController
-public class UserController {
+public class ApiController {
 
     @GetMapping(value = "/api/test/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @CrossOrigin
